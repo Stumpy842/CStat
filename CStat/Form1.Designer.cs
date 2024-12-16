9580bda6bd858ef5100b11d252498eb2fe536406
Steven Stover <stumpy842@gmail.com>
2024-12-16 16:11:59 -0500

    Add project files.

---
+++ CStat/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace CStat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            gbStatMode = new GroupBox();
+            rbCeiling = new RadioButton();
+            rbWall = new RadioButton();
+            rbSprite = new RadioButton();
+            gbEntryMode = new GroupBox();
+            rbBinary = new RadioButton();
+            rbHex = new RadioButton();
+            rbDecimal = new RadioButton();
+            tbValue = new TextBox();
+            label1 = new Label();
+            btCalc = new Button();
+            LV = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            rtbInfo = new RichTextBox();
+            toolTip1 = new ToolTip(components);
+            btClear = new Button();
+            gbStatMode.SuspendLayout();
+            gbEntryMode.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gbStatMode
+            // 
+            gbStatMode.Controls.Add(rbCeiling);
+            gbStatMode.Controls.Add(rbWall);
+            gbStatMode.Controls.Add(rbSprite);
+            gbStatMode.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            gbStatMode.Location = new Point(22, 26);
+            gbStatMode.Name = "gbStatMode";
+            gbStatMode.Size = new Size(249, 64);
+            gbStatMode.TabIndex = 0;
+            gbStatMode.TabStop = false;
+            gbStatMode.Text = "Mode";
+            // 
+            // rbCeiling
+            // 
+            rbCeiling.AutoSize = true;
+            rbCeiling.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rbCeiling.Location = new Point(133, 28);
+            rbCeiling.Name = "rbCeiling";
+            rbCeiling.Size = new Size(105, 21);
+            rbCeiling.TabIndex = 2;
+            rbCeiling.TabStop = true;
+            rbCeiling.Text = "Ceiling/floor";
+            rbCeiling.UseVisualStyleBackColor = true;
+            rbCeiling.CheckedChanged += rbCeiling_CheckedChanged;
+            // 
+            // rbWall
+            // 
+            rbWall.AutoSize = true;
+            rbWall.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rbWall.Location = new Point(74, 28);
+            rbWall.Name = "rbWall";
+            rbWall.Size = new Size(53, 21);
+            rbWall.TabIndex = 1;
+            rbWall.TabStop = true;
+            rbWall.Text = "Wall";
+            rbWall.UseVisualStyleBackColor = true;
+            rbWall.CheckedChanged += rbWall_CheckedChanged;
+            // 
+            // rbSprite
+            // 
+            rbSprite.AutoSize = true;
+            rbSprite.Checked = true;
+            rbSprite.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rbSprite.Location = new Point(6, 28);
+            rbSprite.Name = "rbSprite";
+            rbSprite.Size = new Size(62, 21);
+            rbSprite.TabIndex = 0;
+            rbSprite.TabStop = true;
+            rbSprite.Text = "Sprite";
+            rbSprite.UseVisualStyleBackColor = true;
+            rbSprite.CheckedChanged += rbSprite_CheckedChanged;
+            // 
+            // gbEntryMode
+            // 
+            gbEntryMode.Controls.Add(rbBinary);
+            gbEntryMode.Controls.Add(rbHex);
+            gbEntryMode.Controls.Add(rbDecimal);
+            gbEntryMode.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            gbEntryMode.Location = new Point(297, 26);
+            gbEntryMode.Name = "gbEntryMode";
+            gbEntryMode.Size = new Size(226, 64);
+            gbEntryMode.TabIndex = 1;
+            gbEntryMode.TabStop = false;
+            gbEntryMode.Text = "Entry";
+            // 
+            // rbBinary
+            // 
+            rbBinary.AutoSize = true;
+            rbBinary.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            rbBinary.Location = new Point(144, 28);
+            rbBinary.Name = "rbBinary";
+            rbBinary.Size = new Size(66, 21);
+            rbBinary.TabIndex = 2;
+            rbBinary.Text = "Binary";
+            rbBinary.UseVisualStyleBackColor = true;
+            rbBinary.CheckedChanged += rbBinary_CheckedChanged;
+            // 
+            // rbHex
+            // 
+            rbHex.AutoSize = true;
+            rbHex.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            rbHex.Location = new Point(88, 28);
+            rbHex.Name = "rbHex";
+            rbHex.Size = new Size(50, 21);
+            rbHex.TabIndex = 1;
+            rbHex.Text = "Hex";
+            rbHex.UseVisualStyleBackColor = true;
+            rbHex.CheckedChanged += rbHex_CheckedChanged;
+            // 
+            // rbDecimal
+            // 
+            rbDecimal.AutoSize = true;
+            rbDecimal.Checked = true;
+            rbDecimal.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            rbDecimal.Location = new Point(6, 28);
+            rbDecimal.Name = "rbDecimal";
+            rbDecimal.Size = new Size(76, 21);
+            rbDecimal.TabIndex = 0;
+            rbDecimal.TabStop = true;
+            rbDecimal.Text = "Decimal";
+            rbDecimal.UseVisualStyleBackColor = true;
+            rbDecimal.CheckedChanged += rbDecimal_CheckedChanged;
+            // 
+            // tbValue
+            // 
+            tbValue.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tbValue.Location = new Point(551, 54);
+            tbValue.Name = "tbValue";
+            tbValue.Size = new Size(163, 25);
+            tbValue.TabIndex = 2;
+            toolTip1.SetToolTip(tbValue, "Use 0x prefix for hex, 0b prefix for binary if desired (not required)\r\nUse spaces in binary input to improve readability if desired");
+            tbValue.WordWrap = false;
+            tbValue.MouseClick += tbValue_MouseClick;
+            tbValue.Enter += tbValue_Enter;
+            tbValue.KeyDown += tbValue_KeyDown;
+            tbValue.MouseEnter += tbValue_MouseEnter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(551, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 21);
+            label1.TabIndex = 3;
+            label1.Text = "Enter value";
+            // 
+            // btCalc
+            // 
+            btCalc.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btCalc.Location = new Point(750, 26);
+            btCalc.Name = "btCalc";
+            btCalc.Size = new Size(271, 27);
+            btCalc.TabIndex = 4;
+            btCalc.Text = "Calculate from checkboxes";
+            btCalc.UseVisualStyleBackColor = true;
+            btCalc.Click += btCalc_Click;
+            // 
+            // LV
+            // 
+            LV.CheckBoxes = true;
+            LV.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader6, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader7 });
+            LV.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LV.FullRowSelect = true;
+            LV.GridLines = true;
+            LV.HeaderStyle = ColumnHeaderStyle.Nonclickable;
+            LV.LabelWrap = false;
+            LV.Location = new Point(22, 110);
+            LV.Name = "LV";
+            LV.Scrollable = false;
+            LV.Size = new Size(1046, 438);
+            LV.TabIndex = 6;
+            LV.UseCompatibleStateImageBehavior = false;
+            LV.View = View.Details;
+            LV.ItemChecked += LV_ItemChecked;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "On";
+            columnHeader1.Width = 36;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Bit";
+            columnHeader6.TextAlign = HorizontalAlignment.Right;
+            columnHeader6.Width = 44;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Hex";
+            columnHeader2.TextAlign = HorizontalAlignment.Right;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Dec";
+            columnHeader3.TextAlign = HorizontalAlignment.Right;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Label";
+            columnHeader4.Width = 200;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Description";
+            columnHeader5.Width = 580;
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "Key";
+            columnHeader7.Width = 50;
+            // 
+            // rtbInfo
+            // 
+            rtbInfo.BackColor = SystemColors.Control;
+            rtbInfo.BorderStyle = BorderStyle.None;
+            rtbInfo.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            rtbInfo.Location = new Point(130, 563);
+            rtbInfo.Name = "rtbInfo";
+            rtbInfo.ReadOnly = true;
+            rtbInfo.ScrollBars = RichTextBoxScrollBars.None;
+            rtbInfo.Size = new Size(825, 58);
+            rtbInfo.TabIndex = 6;
+            rtbInfo.TabStop = false;
+            rtbInfo.Text = "";
+            rtbInfo.WordWrap = false;
+            rtbInfo.LinkClicked += rtbInfo_LinkClicked;
+            // 
+            // btClear
+            // 
+            btClear.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btClear.Location = new Point(750, 67);
+            btClear.Name = "btClear";
+            btClear.Size = new Size(271, 23);
+            btClear.TabIndex = 5;
+            btClear.Text = "Clear checkboxes";
+            btClear.UseVisualStyleBackColor = true;
+            btClear.Click += btClear_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1085, 644);
+            Controls.Add(btClear);
+            Controls.Add(rtbInfo);
+            Controls.Add(LV);
+            Controls.Add(btCalc);
+            Controls.Add(label1);
+            Controls.Add(tbValue);
+            Controls.Add(gbEntryMode);
+            Controls.Add(gbStatMode);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            SizeGripStyle = SizeGripStyle.Hide;
+            Text = "CStat Calculator";
+            Load += Form1_Load;
+            gbStatMode.ResumeLayout(false);
+            gbStatMode.PerformLayout();
+            gbEntryMode.ResumeLayout(false);
+            gbEntryMode.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {}
+
+        #endregion
+
+        private GroupBox gbStatMode;
+        private RadioButton rbWall;
+        private RadioButton rbSprite;
+        private GroupBox gbEntryMode;
+        private RadioButton rbBinary;
+        private RadioButton rbHex;
+        private RadioButton rbDecimal;
+        private TextBox tbValue;
+        private Label label1;
+        private Button btCalc;
+        private ListView LV;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private RadioButton rbCeiling;
+        private RichTextBox rtbInfo;
+        private ToolTip toolTip1;
+        private Button btClear;
+    }
+}
---
+++ CStat/Form1.cs
@@ -0,0 +1,379 @@
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Globalization;
+using System.Security.Policy;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace CStat
+{
+    public partial class Form1 : Form
+    {
+        //public System.Diagnostics.Process p = new System.Diagnostics.Process();
+
+        private bool act = false;
+        private int entryMode;
+        private int statCode;
+        private readonly Regex rHex = new Regex("^0x", RegexOptions.IgnoreCase);
+        private readonly Regex rBin = new Regex("^0b", RegexOptions.IgnoreCase);
+        private readonly Regex rSpace = new Regex(@"\s", RegexOptions.IgnoreCase);
+        private readonly String[] Base = { "decimal", "hex", "binary" };
+        private readonly String[] Limits = { "65536 decimal", "10000 hex", "1 0000 0000 0000 0000 binary" };
+
+        private readonly String[] sLabel = {"BLOCK", "TRANSLUCENT", "XFLIP", "YFLIP",
+            "WALL", "FLOOR", "ONE_SIDED", "YCENTER", "BLOCK_HITSCAN", "TRANSLUCENT_INVERT", "(undefined)",
+            "NOSHADE", "(unused)", "(undefined)", "(undefined)", "INVISIBLE"};
+
+        private readonly String[] sDesc = {"Make sprite blockable", "Make sprite transparent",
+            "Flip sprite around x-axis", "Flip sprite around y-axis", "Draw sprite as vertically flat (wall aligned)",
+            "Draw sprite as horizontally flat (floor aligned)", "Make sprite one sided", "Real centered centering (half submerged)",
+            "Make sprite able to be hit with weapons", "Second transparency level (combine with cstat 2)",
+            "Sprite will take priority being drawn over other sprites (Polymost)", "Sprite will not be forced to take shade of sector " +
+            "(EDuke32 extension)", "(reserved) (used by Shadow Warrior, Blood)", "Sprite will not cast a Polymer shadow (EDuke32)",
+            "Sprite will be invisible but still cast a Polymer shadow (EDuke32)", "Invisible (do not render, skip EVENT_ANIMATESPRITES)"};
+
+        private readonly String[] sKey = { "B", "T", "F", "F", "R", "R", "1", "C", "H", "T", "", "", "", "", "", "" };
+
+        private readonly String[] wLabel = {"BLOCK", "BOTTOM_SWAP", "ALIGN_BOTTOM", "XFLIP",
+            "MASKED", "1WAY", "BLOCK_HITSCAN", "TRANSLUCENT", "YFLIP", "TRANS_FLIP", "YAX_UPWALL",
+            "YAX_DOWNWALL", "ROTATE_90", "(unused)", "BLOCK_ACTOR", "WARP_HITSCAN"};
+
+        private readonly String[] wDesc = { "Make wall blockable", "Make bottoms of invisible walls swapped",
+            "Align picture on bottom", "Flip wall around x-axis", "Make wall masking, two-sided", "Make wall masking, one-sided",
+            "Make wall able to be hit by weapons", "Make wall transparent", "Flip wall around y-axis",
+            "Second transparency level (combine with cstat 128)", "(YAX - EDuke32 extension)", "(YAX - EDuke32 extension)",
+            "Rotate texture by 90 degrees counter-clockwise (EDuke32 extension)", "(reserved)",
+            "(used by Shadow Warrior, Blood) (temp use by editor)", "(used by Shadow Warrior, Blood)"};
+
+        private readonly String[] wKey = { "B", "2", "O", "F", "M", "1", "H", "T", "F", "T", "", "", "", "", "", "" };
+
+        private readonly String[] cLabel = { "PARALLAX", "GROUDRAW", "SWAP_XY", "DOUBLE_SMOOSH", "X_FLIP", "Y-FLIP",
+            "RELATIVITY", "MASKED_FLOOR", "TRANSLUCENCE", "BLOCK", "YAX'ED", "HITSCAN", "(undefined)", "(undefined)", "(undefined)",
+            "(undefined)"};
+
+        private readonly String[] cDesc = {"Enable/disable parallax", "Groudraw (abandoned height mapping feature)",
+            "Swap x & y axes", "Toggle ceiling/floor texture expansion",
+            "Flip the current object around x-axis", "Flip the current object around y-axis", "Align texture to first wall of sector",
+            "Cycles translucence for ceilings/floors for ROR (bits 8-7)", "01=masked  10=transluscent masked  " +
+            "11=reverse transluscent masked", "Blocking ceiling/floor", "YAX'ed ceiling/floor", "Hitscan-sensitive ceiling/floor",
+            "(reserved)", "(reserved)", "(reserved)", "(reserved)"};
+
+        private readonly String[] cKey = { "P", "", "F", "E", "F", "F", "R", "T", "T", "", "", "", "", "", "", "" };
+
+        public Form1()
+        {
+            int entryMode = 0;
+            InitializeComponent();
+            btClear.Enabled = false;
+            SetPretext(entryMode);
+            InitLV();
+            rtbInfo.Text = "Information from https://wiki.eduke32.com/wiki/Cstat_(sprite)," +
+                " https://wiki.eduke32.com/wiki/Cstat_(wall)" + Environment.NewLine +
+                "and https://voidpoint.io/terminx/eduke32/-/blob/master/source/build/include/buildtypes.h";
+        }
+
+        private void InitLV()
+        {
+            int n;
+            ListViewItem item;
+            LV.BeginUpdate();
+            LV.Items.Clear();
+            for (int c = 0; c < 16; c++)
+            {
+                item = new ListViewItem();
+                item.SubItems.Add(c.ToString());
+                n = 1 << c;
+                item.SubItems.Add(n.ToString("X"));
+                item.SubItems.Add(n.ToString());
+                if (rbSprite.Checked)
+                {
+                    item.SubItems.Add(sLabel[c]);
+                    item.SubItems.Add(sDesc[c]);
+                    item.SubItems.Add(sKey[c]);
+                }
+                else if (rbWall.Checked)
+                {
+                    item.SubItems.Add(wLabel[c]);
+                    item.SubItems.Add(wDesc[c]);
+                    item.SubItems.Add(wKey[c]);
+                }
+                else
+                {
+                    item.SubItems.Add(cLabel[c]);
+                    item.SubItems.Add(cDesc[c]);
+                    item.SubItems.Add(cKey[c]);
+                }
+                LV.Items.Add(item);
+            }
+            LV.EndUpdate();
+        }
+
+        private void UpdateLV()
+        {
+            ListViewItem item;
+            LV.BeginUpdate();
+            for (int c = 0; c < 16; c++)
+            {
+                item = LV.Items[c];
+                if (rbSprite.Checked)
+                {
+                    item.SubItems[4].Text = sLabel[c];
+                    item.SubItems[5].Text = sDesc[c];
+                    item.SubItems[6].Text = sKey[c];
+                }
+                else if (rbWall.Checked)
+                {
+                    item.SubItems[4].Text = wLabel[c];
+                    item.SubItems[5].Text = wDesc[c];
+                    item.SubItems[6].Text = wKey[c];
+                }
+                else
+                {
+                    item.SubItems[4].Text = cLabel[c];
+                    item.SubItems[5].Text = cDesc[c];
+                    item.SubItems[6].Text = cKey[c];
+                }
+                LV.Items[c] = item;
+            }
+            LV.EndUpdate();
+        }
+
+        private void setText(int sc)
+        {
+            string PadBinary(string s)
+            {
+                string Reverse(string r)
+                {
+                    char[] chars = r.ToCharArray();
+                    Array.Reverse(chars);
+                    return new string(chars);
+                }
+
+                string t = "";
+                string b = Reverse(s);
+                for (int c = 0; c < b.Length; c++)
+                {
+                    if ((c > 0) && (c % 4 == 0)) t += " ";
+                    t += b[c];
+                }
+                return Reverse(t);
+        }
+            switch (entryMode)
+            {
+                case 0: tbValue.Text = sc.ToString(); break;
+                case 1: tbValue.Text = sc.ToString("X"); break;
+                case 2: tbValue.Text = PadBinary(sc.ToString("B")); break;
+            }
+        }
+
+        private void btCalc_Click(object sender, EventArgs e)
+        {
+            enableClear();
+            statCode = 0;
+            for (int c = 0; c < 16; c++)
+            {
+                if (LV.Items[c].Checked)
+                {
+                    statCode += 1 << c;
+                    LV.Items[c].Selected = true;
+                }
+                else
+                { LV.Items[c].Selected = false; }
+            }
+            setText(statCode);
+        }
+
+        private void SetPretext(int em)
+        {
+            tbValue.PlaceholderText = "Enter a " + Base[em] + " number";
+        }
+
+
+        private void rbChanged()
+        {
+            entryMode = SetEntryMode();
+             if (tbValue.Text != "") setText(statCode);
+            SetPretext(entryMode);
+        }
+
+        private void rbDecimal_CheckedChanged(object sender, EventArgs e)
+        {
+            rbChanged();
+        }
+
+        private void rbHex_CheckedChanged(object sender, EventArgs e)
+        {
+            rbChanged();
+        }
+
+        private void rbBinary_CheckedChanged(object sender, EventArgs e)
+        {
+            rbChanged();
+        }
+
+        private int SetEntryMode()
+        {
+            int r;
+            if (rbDecimal.Checked) r = 0;
+            else if (rbHex.Checked) r = 1;
+            else r = 2;
+            return r;
+        }
+
+        private void clrChecked()
+        {
+            btClear.Enabled = false;
+            foreach (ListViewItem i in LV.CheckedItems)
+            {
+                i.Checked = false;
+                i.Selected = false;
+            }
+        }
+
+        private void DoCalc()
+        {
+            long ParseText(string t)
+            {
+                long sc = 0;
+                switch (entryMode)
+                {
+                    case 0: // decimal
+                        {
+                            if (!long.TryParse(t, NumberStyles.Integer,
+                                    CultureInfo.InvariantCulture, out sc))
+                                sc = -1;
+                        }
+                        break;
+                    case 1: // hex
+                        {
+                            if (!long.TryParse(t, NumberStyles.HexNumber,
+                                    CultureInfo.InvariantCulture, out sc))
+                                sc = -2;
+                        }
+                        break;
+                    case 2: // binary
+                        {
+                            if (!long.TryParse(t, NumberStyles.BinaryNumber,
+                                    CultureInfo.InvariantCulture, out sc))
+                                sc = -3;
+                        }
+                        break;
+                }
+                return sc;
+            }
+
+            string txt = tbValue.Text;
+            switch (entryMode)
+            {
+                case 0: txt = txt.Trim(); break;
+                case 1: txt = rHex.Replace(txt, string.Empty); break;
+                case 2: txt = rBin.Replace(rSpace.Replace(txt, string.Empty), string.Empty); break;
+            }
+
+            if (String.IsNullOrEmpty(txt))
+            {
+                MessageBox.Show("Please enter a " + Base[entryMode] + " number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            long pt = (ParseText(txt));
+            if (pt < 0)
+            {
+                MessageBox.Show("Invalid or too large " + Base[entryMode] + " number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbValue.Clear();
+                clrChecked();
+                statCode = 0;
+                return;
+            }
+            // process pt and display results
+            statCode = (int)(pt % 65536);
+            if (statCode != pt)
+            {
+                MessageBox.Show(txt + " too large" + Environment.NewLine + "Must be less than " + Limits[entryMode], "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                btClear_Click(this, EventArgs.Empty);
+                return;
+            }
+            int a; bool b;
+            for (int c = 0; c < 16; c++)
+            {
+                a = 1 << c;
+                b = ((statCode & a) == a);
+                LV.Items[c].Checked = b;
+                LV.Items[c].Selected = b;
+            }
+            enableClear();
+        }
+
+        private void tbValue_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                DoCalc();
+            }
+        }
+
+        private void rbSprite_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLV();
+        }
+
+        private void rbWall_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLV();
+        }
+
+        private void rbCeiling_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLV();
+        }
+
+        private void enableClear()
+        {
+            btClear.Enabled = (LV.CheckedItems.Count > 0);
+        }
+
+        // Event raised from RichTextBox when user clicks on a link
+        private void rtbInfo_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            bool IsHttpURL(string u)
+            { return ((!string.IsNullOrWhiteSpace(u)) && (u.StartsWith("http", StringComparison.CurrentCultureIgnoreCase))); }
+
+            string url = e.LinkText!;
+            if (IsHttpURL(url))
+            { using Process p = Process.Start(new ProcessStartInfo(url) { UseShellExecute = true })!; }
+        }
+
+        private void btClear_Click(object sender, EventArgs e)
+        {
+            tbValue.Clear();
+            clrChecked();
+            statCode = 0;
+        }
+
+        private void tbValue_Enter(object sender, EventArgs e)
+        {
+            tbValue.SelectAll();
+        }
+
+        private void tbValue_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!act)
+                tbValue.SelectAll();
+            act = true;
+        }
+
+        private void tbValue_MouseEnter(object sender, EventArgs e)
+        {
+            act = tbValue.Focused;
+        }
+
+        private void LV_ItemChecked(object sender, ItemCheckedEventArgs e)
+        {
+            enableClear();
+        }
+    }
+}
